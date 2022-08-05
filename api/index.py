a7f8a803eff3dacf9b36492eae6030e70fa546ab
Hoyou06 <88663820+Hoyou06@users.noreply.github.com>
2022-08-06 07:54:04 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,25 +1,10 @@
-# from http.server import BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler
  
  
-# class handler(BaseHTTPRequestHandler):
+class handler(BaseHTTPRequestHandler):
  
-#     def do_GET(self):
-#         self.send_response(200)
-#         self.send_header('Content-type', 'text/plain')
-#         self.end_headers()
-#         self.wfile.write("DearXuan's API by python!".encode())
-#         return
-import flask,json
-@api.route('/article',methods=['post']) 
-def article():
-  #url格式参数?id=12589&name='lishi'
-  id = flask.request.args.get('id')
-    
-  if id:
-    if id == '12589':
-      ren = {'msg':'成功访问文章','msg_code':200}
-    else:
-      ren = {'msg':'找不到文章','msg_code':400}
-  else:
-    ren = {'msg':'请输入文章id参数','msg_code':-1}
-  return json.dumps(ren,ensure_ascii=False)
+    def do_GET(self):
+        self.send_response(200)
+        return("123")
+        self.wfile.write("DearXuan's API by python!".encode())
+        return
