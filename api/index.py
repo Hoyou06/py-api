6221a1281c929733b5a074d86e0d37df2bce94b3
Hoyou06 <88663820+Hoyou06@users.noreply.github.com>
2022-08-06 17:09:50 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,6 +5,8 @@ class handler(BaseHTTPRequestHandler):
  
     def do_GET(self):
         self.send_response(200)
-        
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        id=id+1
         self.wfile.write("DearXuan's API by python!".encode())
-        return
+        return (id)
